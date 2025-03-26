459e799c0ffa2011165aae842033851367830f7c
Marcelo Sette <msettejunior@gmail.com>
2025-03-26 16:01:54 -0300

    Correção ortográfica do Titulo e Correção da consulta default do app.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from model.model import generate_response
 
-st.title("🏦 BDI (Banco de Dados Intelligence)")
+st.title("🏦 BDI (Banco de Dados Inteligente)")
 
 # 🎛️ Interface no Streamlit
 with st.form("query_form"):
-    user_input = st.text_area("Digite sua pergunta:", "Qual foi o último valor do IPCA em Recife?")
+    user_input = st.text_area("Digite sua pergunta:", "Quais são as tabelas contidas no Bando de Dados?")
     submitted = st.form_submit_button("Consultar")
 
     if submitted:
